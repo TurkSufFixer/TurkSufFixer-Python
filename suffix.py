ab1573196a7e8ad54f4f434ce5020706ba3b18c7
talha252 <talhacolakoglu@gmail.com>
2016-03-17 20:49:05 +0200

    Change the file handling behaviour

--- suffix.py
+++ suffix.py
@@ -18,17 +18,16 @@ class Suffix:
     tens =    {'1':u'on','2':u'yirmi','3':u'otuz','4':u'kırk','5':u'elli','6':u'altmış','7':u'yetmiş','8':u'seksen','9':u'doksan'}
     digits =  {2:u'yüz',3:u'bin',6:u'milyon', 9:u'milyar',12:u'trilyon',15:u'katrilyon'}
 
-    def __init__(self, dictionary="sozluk/isim.itu", exceptions="sozluk/istisna.itu", haplo="sozluk/unludus.itu", poss="sozluk/ihali.itu"):
-        # TODO: safdece possfile yi açık tut diğerlerini kapat çünkü güncellemeyeceksin
-        self.dictfile   = io.open(dictionary,"r+",encoding='utf-8')
-        self.exceptfile = io.open(exceptions,"r+",encoding='utf-8')
+    def __init__(self, dictpath="sozluk/isim.itu", exceptions="sozluk/istisna.itu", haplopath="sozluk/unludus.itu", poss="sozluk/ihali.itu"):
         self.possfile   = io.open(poss,      "r+",encoding='utf-8')
-        self.haplofile  = io.open(haplo,     "r+",encoding='utf-8')
-
-        self.exceptions = set(self.exceptfile.read().split('\n'))
         self.possessive = set(self.possfile.read().split('\n'))
-        self.haplology  = set(self.haplofile.read().split('\n'))
-        self.dictionary = set(self.dictfile.read().split('\n')) | self.exceptions | self.haplology
+        with io.open(dictpath,  "r+",encoding='utf-8') as dictfile,  \
+             io.open(exceptions,"r+",encoding='utf-8') as exceptfile, \
+             io.open(haplopath, "r+",encoding='utf-8') as haplofile:
+
+            self.exceptions = set(exceptfile.read().split('\n'))
+            self.haplology  = set(haplofile.read().split('\n'))
+            self.dictionary = set(dictfile.read().split('\n')) | self.exceptions | self.haplology
 
     def _readNumber(self, number):
         if len(number) == 1 and number[-1] == '0': return u'sıfır'
@@ -141,17 +140,14 @@ class Suffix:
                 suffix = suffix.replace('D','t')
             else:
                 suffix = suffix.replace('D','d')
-
         # and finally add buffer letter, if we added n buffer letter before this code will discarded
         if name[-1] in self.vowels and suffix[0] in self.vowels:
             suffix = 'y' + suffix
-
+            
         return suffix
+
     def __del__(self):
-        self.dictfile.close()
-        self.exceptfile.close()
         self.possfile.close()
-        self.haplofile.close()
 
 class NotInSuffixes(Exception):
     pass
