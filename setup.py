e0351bf306ddd0609393053d718092575c346323
Talha Çolaoğlu <talhacolakoglu@gmail.com>
2018-12-12 22:15:47 +0300

    Create package & add setup configs

---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- coding: UTF-8 -*-
+from setuptools import setup
+
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='turksuffixer',
+    version='1.0',
+    description='Suffix generator for Turkish language',
+    long_description=readme,
+    url='https://github.com/TurkSufFixer/TurkSufFixer-Python',
+    author='Talha Çolakoğlu',
+    author_email='talhacolakoglu@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'
+    ],
+    keywords='turkish suffix',
+    packages=['turksuffixer'],
+    package_data={'data': ['sozluk/*']},
+)
